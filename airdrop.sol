7dd807d576785221d54be70213518ed30e16a9b8
FelipeGeazi <felipegeaziartes@gmail.com>
2022-06-09 17:13:54 -0300

    Finalizado

--- airdrop.sol
+++ airdrop.sol
@@ -14,7 +14,8 @@ contract Airdrop  {
     // Structs
 
     // Enum
-    enum Status { ACTIVE, PAUSED, CANCELLED } // mesmo que uint8
+    enum Status { ACTIVE, PAUSED, CANCELLED } //ACTIVE=0, PAUSED=1, CANCELLED=2 -- uint8
+
 
 
     // Properties
@@ -36,14 +37,18 @@ contract Airdrop  {
     constructor(address token) {
         owner = msg.sender;
         tokenAddress = token;
-        contractState = Status.PAUSED;
+        contractState = Status.ACTIVE;
     }
 
 
-    // Public Functions
-
+    // Public Function
     function subscribe() public returns(bool) {
         //TODO: Need Implementation
+
+        require(hasSubscribed(msg.sender) == false, "Carteira ja inscrita !");
+        subscribers.push(msg.sender);
+
+        return true;
 
     }
 
@@ -51,6 +56,7 @@ contract Airdrop  {
 
         uint256 balance = CryptoToken(tokenAddress).balanceOf(address(this));
         uint256 amountToTransfer = balance / subscribers.length;
+      
         for (uint i = 0; i < subscribers.length; i++) {
             require(subscribers[i] != address(0));
             require(CryptoToken(tokenAddress).transfer(subscribers[i], amountToTransfer));
@@ -60,19 +66,34 @@ contract Airdrop  {
     }
 
     function state() public view returns(Status) {
-        return contractState;
+        return contractState;   
     }
 
 
     // Private Functions
-    function hasSubscribed(address subscriber) private returns(bool) {
+    function hasSubscribed(address subscriber) private view returns(bool) {
         //TODO: Need Implementation
+       for (uint i = 0; i< subscribers.length; i++){
+           if(subscribers[i] == subscriber){
+               return true;
+           }
+
+       }
+        return false;
     }
+
+      function getkillBalance() external view returns (uint){
+        return address(this).balance;}
 
     // Kill
     function kill() public isOwner {
         //TODO: Need Implementation
-    }
+         
     
+        if(state() == Status.ACTIVE || state() == Status.PAUSED ){
+        contractState = Status.CANCELLED;}
+        selfdestruct(payable(msg.sender));
+    }
+
     
 }
